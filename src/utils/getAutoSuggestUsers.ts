594cf4dbb131bca01e1bc7f32333fdb308b3913b
mrinex <mrinex@mail.ru>
2021-04-05 14:34:19 +0300

    fix: search anywhere in a string

--- src/utils/getAutoSuggestUsers.ts
+++ src/utils/getAutoSuggestUsers.ts
@@ -3,7 +3,7 @@ import { OutputUser } from '../users/user.model';
 function getAutoSuggestUsers(
   memoryUsers: OutputUser[], loginSubstring: string, limit: number
   ): OutputUser[] {
-  const regex = new RegExp(`^${loginSubstring.toLowerCase()}`);
+  const regex = new RegExp(`${loginSubstring.toLowerCase()}`);
   
   const filteredUsers = memoryUsers.filter((user) => {
     return regex.test(user.login.toLowerCase()) && !user.isDeleted;
