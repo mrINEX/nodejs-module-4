e0c58a89f5edefb1a6cb862e37f5fa5cf277abdd
unknown <mrinex@mail.ru>
2021-04-27 23:10:11 +0300

    feat: add log errors to groups and users-id-group-id

--- src/modules/groups/group.router.ts
+++ src/modules/groups/group.router.ts
@@ -1,13 +1,15 @@
 import { Router } from 'express';
+
 import * as api from './group.controllers';
 import { schemaInputGroup, validate } from './utils/validate-and-schemas';
+import { errorHandling } from '../../common/error_handling';
 
 const router = Router();
 
-router.route('/').post(validate(schemaInputGroup), api.postMethodHandler);
-router.route('/:id').get(api.getMethodHandler);
-router.route('/').get(api.getAllMethodHandler);
-router.route('/:id').put(validate(schemaInputGroup), api.putMethodHandler);
-router.route('/:id').delete(api.deleteMethodHandler);
+router.route('/').post(validate(schemaInputGroup), errorHandling(api.postMethodHandler));
+router.route('/:id').get(errorHandling(api.getMethodHandler));
+router.route('/').get(errorHandling(api.getAllMethodHandler));
+router.route('/:id').put(validate(schemaInputGroup), errorHandling(api.putMethodHandler));
+router.route('/:id').delete(errorHandling(api.deleteMethodHandler));
 
 export { router };
--- src/modules/users_id_group_id/index.ts
+++ src/modules/users_id_group_id/index.ts
@@ -1,8 +1,10 @@
 import { Application } from 'express';
+
 import { addUsersToGroupMiddleware } from './add_users_to_group.controllers';
+import { errorHandling } from '../../common/error_handling';
 
 function registerModule(app: Application, url: string): void {
-  app.use(url, addUsersToGroupMiddleware);
+  app.use(url, errorHandling(addUsersToGroupMiddleware));
 }
 
 export default registerModule;
